598f99625025cfb55e6a80759ca38be8c8f871bf
Your Name <you@example.com>
2024-04-16 09:29:32 +0100

    initialize new expense modal

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      maxLength: 50,
+      decoration: InputDecoration(label: Text('Title')),
+    );
+  }
+}
