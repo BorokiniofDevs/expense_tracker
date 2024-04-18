5585c5619248588604d6de18bf82525ec2db9dcf
Your Name <you@example.com>
2024-04-18 13:48:04 +0100

    implement auto dark color mode from colorscheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,60 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 82, 250),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 224, 18, 35),
+);
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    darkTheme: ThemeData().copyWith(
+      colorScheme: kDarkColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer),
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondary,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+        ),
+      ),
+      textTheme: const TextTheme().copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: kDarkColorScheme.onSecondaryContainer,
+          fontSize: 14,
+        ),
+      ),
+    ),
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primaryContainer,
+          foregroundColor: kColorScheme.onPrimaryContainer),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondary,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      textTheme: const TextTheme().copyWith(
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.normal,
+          color: kColorScheme.onSecondaryContainer,
+          fontSize: 14,
+        ),
+      ),
+    ),
+    home: const Expenses(),
   ));
 }
